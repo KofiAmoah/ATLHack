a8f942ca1eba1eed73b5a1d39844f6383e061444
bdotu <kofi.dotu@gmail.com>
2014-11-08 19:17:26 -0600

    testing pages

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -6,6 +6,7 @@ class TwilioController < ApplicationController
 
   def send_sms
   	message = params[:message]
+    subject = params[:subject]
   	number = params[:number]
   	account_sid = 'AC033a2b457424bb3af29cd54ea39644de'
   	auth_token = 'f88d97c71de5a9fd9463202ea75b88b6'
@@ -14,7 +15,13 @@ class TwilioController < ApplicationController
 
   	@message = @client.account.messages.create({:to => "+1"+"#{number}",
   																:from => "+16147790912",
-  																:body => "#{message}"})
+  																:body => "Subj: "+"#{subject}"+" "+"Msg: "+"#{message}"})
   	redirect_to '/'
   end
+
+  # def send_verification
+  #   number = params[:number]
+  #   verification_code = (1000...9999).to_a.shuffle.first
+
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'twilio/index'
+  # get 'twilio/index'
 
   root :to => 'twilio#index'
   post '/send_sms' => 'twilio#send_sms'
